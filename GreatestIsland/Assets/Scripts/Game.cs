7e287517bf63611662bf36de774b4b5d3f465b94
mi18069 <mi18069@alas.matf.bg.ac.rs>
2024-11-06 21:38:29 +0100

    Refactored code between camera and http client

--- GreatestIsland/Assets/Scripts/Game.cs
+++ GreatestIsland/Assets/Scripts/Game.cs
@@ -1,19 +1,15 @@
+using System;
 using System.Linq;
+using System.Net.Http;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Game : MonoBehaviour
 {
     private Board board;
     private Map map;
-
-    private int[,] defaultMatrix = {{0, 0, 240, 245, 180 },
-                                    {50, 0, 0, 200, 180 },
-                                    {600, 70, 0, 0, 100 },
-                                    {500, 0, 0, 0, 30 },
-                                    {0, 0, 500, 0, 0 },
-                                    {0, 150, 200, 50, 0 },
-                                    {100, 0, 100, 0, 0 },
-                                    {200, 0, 30, 0, 300 } } ;
+    private HttpClient client;
+    private CameraManipulation cameraManipulation;
 
     private Cell currentCell = Cell.InvalidCell;
     private Island currentIsland = Island.InvalidIsland;
@@ -22,6 +18,8 @@ public class Game : MonoBehaviour
     private void Awake()
     {
         board = GetComponentInChildren<Board>();
+        client = GetComponent<HttpClient>();
+        cameraManipulation = GetComponent<CameraManipulation>();
     }
 
     private void Start()
@@ -30,22 +28,13 @@ public class Game : MonoBehaviour
     }
     private void NewGame()
     {
-        var matrix = defaultMatrix;
+        var matrix = client.GetNewMatrix();
         map = new Map();
         map.CreateMapFromMatrix(matrix);
-        CenterCameraToMap(map);
         board.Draw(map);
+        cameraManipulation.AdjustCameraToTilemap(Camera.main, board.cellTilemap);
 
-    }
-
-
-
-    private void CenterCameraToMap(Map map)
-    {
-        var camera = Camera.main;
-        camera.transform.position = new Vector3(map.height / 2f, map.width / 2f, -10f);
-        camera.transform.rotation = Quaternion.Euler(0, 0, 90);
-    }
+    } 
 
     private void Update()
     {
@@ -96,17 +85,8 @@ public class Game : MonoBehaviour
     }
 
     private void ClickedCell()
-    {
-        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3Int cellPosition = board.cellTilemap.WorldToCell(worldPosition);
-        Cell cell = map.GetCell(cellPosition.x, cellPosition.y);
-
-        if (cell.type == Cell.Type.Invalid || cell.type == Cell.Type.Water)
-        {
-            return;
-        }
-
-        Island island = map.GetCellIsland(cell);
+    { 
+        Island island = currentIsland;
         if (island.state != Island.State.Default && island.state != Island.State.Selected)
             return;
 
